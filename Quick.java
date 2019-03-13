f2f4a7db614d9df716461c8c37ffe73263d94418
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-13 10:13:23 -0400

    Made it easier to look at

--- Quick.java
+++ Quick.java
@@ -9,101 +9,48 @@ public class Quick{
  *@return the index of the final position of the pivot element.
  */
 public static int partition (int [] data, int start, int end){
-
-    if (data.length == 1)
-    return 0;
-//[1,9,2,3,5,6]
-int random = (int)(Math.random() * (data.length - 1));
-// picks 3
-int holder = data [0];
-data [0] = data [random];
-data [random] = holder;
-// System.out.println(data [random] + "random");
-//[3,9,2,1,5,6]
-
-return helper (data, start, end);
+    if (data.length == 1){
+        return 0;
+    }
+    int random = (int)(Math.random() * (data.length - 1));
+    int holder = data [0];
+    data [0] = data [random];
+    data [random] = holder;
+    return helper (data, start, end);
 }
+
 public static int helper (int [] data, int start, int end){
-    String d = "";
-    for (int x = 0; x < data.length; x ++){
-        d = d + ", " + data [x];
-    }
-    // System.out.println(d);
-    // System.out.println("s" + start);
-    //     System.out.println("e" + end);
     while (start != end){
-        // System.out.println(start + "s");
-        // System.out.println(end + "e");
-        //     System.out.println( "----------------");
-    //[3,9,2,1,5,6] s1 e5
-        // [3,6,2,1,5,9] s1 e4
-            // [3,5,2,1,6,9] s1 e3
-                // [3,1,2,5,6,9] s2 e3
-                    // [3,1,2,5,6,9]
-                        // System.out.println("help");
-    if (data [start] >= data [0]){
-        int holder = data [start];
-        data [start] = data [end];
-        data [end] = holder;
-        end --;
-    }
-
-    // [3,6,2,1,5,9] s1 e4
-        // [3,5,2,1,6,9] s1 e3
-            // [3,1,2,5,6,9] s1 e3
-                // [3,1,2,5,6,9] s2 e3
-                    // [3,1,2,5,6,9]
-    else{
-        if (data [start] < data [0]){
-            start ++;
-
-    }
-}
-}
-    // [3,6,2,1,5,9] s1 e4
-        // [3,5,2,1,6,9] s1 e3
-            // [3,1,2,5,6,9] s2 e3
-                // [3,1,2,5,6,9] s3 e3
-        if (data [0] > data[start]){
-            // System.out.println(start + "sf");
-            // System.out.println(end + "ef");
-            int holder = data [0];
-            data [0] = data [start];
-            data [start] = holder;
-            // d = "";
-            // for (int x = 0; x < data.length; x ++){
-            //     d = d + ", " + data [x];
-            // }
-            // System.out.println(d);
-            return start;
+        if (data [start] >= data [0]){
+            int holder = data [start];
+            data [start] = data [end];
+            data [end] = holder;
+            end --;
         }
         else{
-            // System.out.println(start + "sf");
-            // System.out.println(end + "ef");
-            int holder = data [0];
-            if (start -1 >= 0){
-            data [0] = data [start - 1];
-            data [start - 1] = holder;
-            // d = "";
-            // for (int x = 0; x < data.length; x ++){
-            //     d = d + ", " + data [x];
-            // }
-            // System.out.println(d);
-            return start - 1;
-            }
-            else{
-                return 0;
+            if (data [start] < data [0]){
+            start ++;
             }
         }
+    }
+    if (data [0] > data[start]){
+        int holder = data [0];
+        data [0] = data [start];
+        data [start] = holder;
+        return start;
+    }
+    else{
+        int holder = data [0];
+        if (start -1 >= 0){
+            data [0] = data [start - 1];
+            data [start - 1] = holder;
+            return start - 1;
+        }
+        else{
+            return 0;
+        }
+    }
 }
-
-        // [3,6,2,1,5,9] s1 e4
-            // [3,5,2,1,6,9] s1 e3
-                // [3,1,2,5,6,9] s2 e3
-                    // [2,1,3,5,6,9] s2
-
-    /*return the value that is the kth smallest value of the array.
- */
  public static int quickselect(int[] data, int k){
      int start = 0;
      int end = data.length;
